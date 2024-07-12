87e0aa4435cada30b63c4eb57dd1bd40eb71f1aa
Amogh <amogh.maheshwari@email.com>
2024-07-12 12:10:52 -0700

    Added some new colors and beautification to the game

--- js/app.js
+++ js/app.js
@@ -16,5 +16,87 @@
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+let board;
+let turn;
+let winner;
+let tie;
+
+const squareEls = document.querySelectorAll('.sqr');
+const messageEl = document.getElementById('message');
+const resetBtnEl = document.getElementById('reset');
+
+const winningCombos = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
+];
+
+function init() {
+  console.log('Initializing the game...');
+  board = ['', '', '', '', '', '', '', '', ''];
+  turn = 'X';
+  winner = false;
+  tie = false;
+  updateBoard();
+  updateMessage();
+}
+
+function updateBoard() {
+    board.forEach((currVal, index) => {
+    squareEls[index].textContent = currVal;
+    });
+}
+
+function updateMessage() {
+    if (winner) {
+        messageEl.textContent = `${turn} wins!`;
+    } 
+    else if (tie) {
+        messageEl.textContent = "It's a tie!";
+    } 
+    else {
+        messageEl.textContent = `It's ${turn}'s turn`;
+    }
+}
+
+function handleClick(evt) {
+    const squareIndex = parseInt(evt.target.id);
+    if (board[squareIndex] !== '' || winner) return;
+
+    board[squareIndex] = turn
+    evt.target.setAttribute('data-player', turn);
+    checkForWinner();
+    checkForTie();
+    switchPlayerTurn();
+    updateBoard();
+    updateMessage();
+}
+
+function checkForWinner() {
+  winningCombos.forEach(combo => {
+    if (board[combo[0]] &&
+        board[combo[0]] === board[combo[1]] &&
+        board[combo[1]] === board[combo[2]]) 
+    {
+      winner = true;
+    }
+  });
+}
+
+function checkForTie() {
+  if (!winner && !board.includes('')) {
+    tie = true;
+  }
+}
+
+function switchPlayerTurn() {
+  if (winner) return;
+  turn = turn === 'X' ? 'O' : 'X';
+}
+
+squareEls.forEach(sqr => sqr.addEventListener('click', handleClick));
+resetBtnEl.addEventListener('click', init);
+
+init();
 
 
